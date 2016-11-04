eba3ff911c585218aafcb74e390cdcef01b2d356
Charles Chen <charles@goforward.com>
2016-11-03 20:45:16 -0700

    make workflows request

--- app/constants/AppConstants.js
+++ app/constants/AppConstants.js
@@ -1,6 +1,13 @@
+var _getOrigin = function() {
+  return (typeof(window) !== 'undefined') ? window.location.origin : Config.url;
+};
+
+var _origin = _getOrigin();
+
 const AppConstants = {
-  WORKFLOW_API_URL: '/api/workflow',
-  CHART_API_URL: '/api/chart',
+
+  WORKFLOW_API_URL: _origin + '/api/workflow',
+  CHART_API_URL: _origin + '/api/chart',
 };
 
 module.exports = AppConstants;
