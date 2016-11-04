a5bed78407eb17b8293d40ea4088db9511d6bdab
Charles Chen <charles@goforward.com>
2016-11-03 21:23:07 -0700

    proxy requests

--- server/index.js
+++ server/index.js
@@ -9,9 +9,55 @@ const isDev = process.env.NODE_ENV !== 'production';
 const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
 const resolve = require('path').resolve;
 const app = express();
+const bodyParser = require('body-parser');
+const request = require('request');
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
+
+const ServerConfig = {
+  workflow: 'http://localhost:5001',
+  chart: 'http://localhost:4001',
+}
+
+var _options = function(req, url) {
+  return {
+    url: url,
+    method: req.method,
+    headers: req.headers,
+    qs: req.query,
+    qsStringifyOptions: { arrayFormat: 'repeat' },
+    body: JSON.stringify(req.body)
+  }
+};
+
+// proxy requests to Workflow API
+app.use('/api/workflow', bodyParser.json(), function(req, res) {
+  const url = ServerConfig.workflow + req.url;
+  request(_options(req, url), function(error, response, body) {
+    if (error) {
+      res.status(500).send(error).end();
+    } else {
+      let bodyJSON = JSON.parse(body);
+      bodyJSON.statusCode = response.statusCode;
+      res.status(response.statusCode).send(bodyJSON).end();
+    }
+  });
+});
+
+// proxy requests to Chart API
+app.use('/api/chart', bodyParser.json(), function(req, res) {
+  const url = ServerConfig.chart + req.url;
+  request(_options(req, url), function(error, response, body) {
+    if (error) {
+      res.status(500).send(error).end();
+    } else {
+      let bodyJSON = JSON.parse(body);
+      bodyJSON.statusCode = response.statusCode;
+      res.status(response.statusCode).send(bodyJSON).end();
+    }
+  });
+});
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
